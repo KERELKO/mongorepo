2d0c2cb073302c7954eb506e0613a41fec554e67
artorias <kerelkobarabash@gmail.com>
2024-06-01 14:33:13 +0300

    Initial commit

---
+++ conf.py
@@ -0,0 +1,7 @@
+import pymongo
+
+mongo_uri = 'mongodb://mongodb:27017/'
+client = pymongo.MongoClient(mongo_uri)
+users_db = client['users_db']
+users = users_db['users']
+users.insert_one({'username': 'admin'})
