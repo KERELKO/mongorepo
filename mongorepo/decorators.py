2b96a6e7356645309c8623e43d7ad9e504e04384
artorias <kerelkobarabash@gmail.com>
2024-08-08 13:33:31 +0300

    Add doc to implements decorator

--- mongorepo/decorators.py
+++ mongorepo/decorators.py
@@ -65,7 +65,35 @@ def mongo_repository(
 
 
 def implements(base_cls: type, **methods: str | Callable) -> Callable:
-    """## Not implemented yet"""
+    """
+    Decorator that allows to implement methods of `base_cls`
+    you can specify them in `Meta` class of decorated class or in `**methods`
+
+    Example:
+
+    ```
+    class Repo:
+        class Meta:
+            subsitude = {'get': 'get_my_entity'}
+    ```
+
+    #### Decorator usage example:
+
+    ```
+    class A:
+        def get_my_entity(self, id: str):
+            ...
+
+    @implements(A, get=A.get_my_entity)
+    class MongoRepo:
+        class Meta:
+            dto = SimpleDTO
+            collection = random_collection()
+
+    my_entity = MongoRepo().get_my_entity(id="10")
+    ```
+
+    """
     def wrapper(cls) -> type:
         return _handle_implements(
             base_cls=base_cls,
